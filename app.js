45c07fc41dc5ac897741c68141f91b4686edbcd4
Subhedar <csubheda@bmc.com>
2022-05-02 15:41:05 +0530

    Add logs

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ import 'dotenv/config'
 import express from "express";
 import { insertSql, createSql } from "./generateSql.js";
 import { pool } from "./db.js";
+import cors from "cors";
  
 // INSERT INTO PROMETHEUS_METADATA_MAPPING(
 //     id, unique_name, type, prefix, tenant_id, is_root_type, is_folder, folder_name, device_mapping_enabled, device_mapping_precedence, p_entity_metric_names, p_entity_metric_labels, p_entity_display_name_metric_labels, p_parent_entity_metric_labels, p_entity_agent_id_labels, p_device_metric_names, p_device_metric_labels, agent_type, graph_by_default, kpi, supported_metrics, version, revision,release, parent_id)
 //     VALUES ('node_node','node_node', 'Node', 'node', '${tenantId}',true, true, 'Node', true, 0, 'node_uname_info', null, null, null, 'instance', 'node_uname_info', 'nodename','Exporter', null, null, 'node_uname_info', '20.08', '20.08.00', '00', null);
 const app = express();
+// var cors = require('cors')
+
+app.use(cors());
 app.use(express.json());
 
 
@@ -14,7 +18,9 @@ app.use(express.json());
     try {
         const query = createSql("PROMETHEUS_METADATA_MAPPING");
         await pool.query(query);
-        app.listen(3000);
+        app.listen(5000, () => {
+            console.log("Server started on port 5000");
+        });
     } catch (error) {
         console.log(error);
     }
@@ -34,11 +40,15 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/insert", async (req, res) => {
+    console.log("Insert api called");
+    console.log(req.body);
     try {
+        console.log("Entered try");
         const query = insertSql(req.body, "PROMETHEUS_METADATA_MAPPING");
         await pool.query(query);
         res.status(200).send("Data Inserted");
     } catch (error) {
+        console.log("Entered catch");
         console.log(error);
         res.status(404).send(error);
     }
--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 import pg from "pg";
 const { Pool } = pg;
 
+// console.log(process.env.DB_HOST);
 const pool = new Pool({
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_DATABASE,
     host : process.env.DB_HOST,
+    
     port : process.env.DB_PORT,
     ssl : {
         rejectUnauthorized : false
